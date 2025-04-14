e06c2f162c3a0b370a5d080df3b7c93da6e410c5
HyeonJooo <parkhj2416@gmail.com>
2025-04-14 15:12:46 +0900

    [섹션5] 인터페이스 기반 프록시 - 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,17 +2,22 @@ package hello.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 //@Configuration도 내부에 @Component가 있기 때문에 자동으로 컴포넌트 스캔 대상이 된다.
 //하지만 나중에 Appv2Config처럼 config를 바꿔서 등록할 것이기 때문에, scanBasePackages를 app으로만 되게 변경한 뒤
 //Import를 이용해 원하는 Config를 사용하기 위해 아래와 같이 어노테이션을 붙임.
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
@@ -20,4 +25,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
