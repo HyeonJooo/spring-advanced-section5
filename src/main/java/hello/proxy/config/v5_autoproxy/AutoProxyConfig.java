0cb179bb5c51544aeda1ef654c99ac54e0f6a387
HyeonJooo <parkhj2416@gmail.com>
2025-06-02 23:42:55 +0900

    [섹션8] 스프링이 제공하는 빈 후처리기2

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package hello.proxy.config.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -16,11 +17,35 @@ import hello.proxy.trace.logtrace.LogTrace;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-	@Bean
+	//@Bean
 	public Advisor advisor1(LogTrace logTrace) {
 		//pointcut
 		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
 		pointcut.setMappedNames("request*", "order*", "save*");
+
+		//advice
+		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+
+		return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+	}
+
+	//@Bean
+	public Advisor advisor2(LogTrace logTrace) {
+		//pointcut
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+
+		//advice
+		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+
+		return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+	}
+
+	@Bean
+	public Advisor advisor3(LogTrace logTrace) {
+		//pointcut
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 
 		//advice
 		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
