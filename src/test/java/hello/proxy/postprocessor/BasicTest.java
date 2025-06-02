c242f09fd11a6a40758f9fc7eccb42aebcc804a7
HyeonJooo <parkhj2416@gmail.com>
2025-06-02 22:32:22 +0900

    [섹션8] 빈 후처리기 - 예제 코드2

--- src/test/java/hello/proxy/postprocessor/BasicTest.java
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -2,7 +2,9 @@ package hello.proxy.postprocessor;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,22 +16,27 @@ public class BasicTest {
 
 	@Test
 	void basicConfig() {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
 
-		//A는 빈으로 등록된다.
-		A a = applicationContext.getBean("beanA", A.class);
-		a.helloA();
+		//beanA 이름으로 B 객체가 등록된다.
+		B b = applicationContext.getBean("beanA", B.class);
+		b.helloB();
 
-		//B는 빈으로 등록되지 않는다.
-		Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+		//A는 빈으로 등록되지 않는다.
+		Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
 	}
 
 	@Slf4j
 	@Configuration
-	static class BasicConfig {
+	static class BeanPostProcessorConfig {
 		@Bean(name = "beanA")
 		public A a() {
 			return new A();
+		}
+
+		@Bean
+		public AtoBPostProcessor atoBPostProcessor() {
+			return new AtoBPostProcessor();
 		}
 	}
 
@@ -44,6 +51,21 @@ public class BasicTest {
 	static class B {
 		public void helloB() {
 			log.info("helloB");
+		}
+	}
+
+	@Slf4j
+	static class AtoBPostProcessor implements BeanPostProcessor {
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+			log.info("beanName={}, bean{}", beanName, bean);
+
+			if (bean instanceof A) {
+				return new B();
+			}
+
+			return bean;
 		}
 	}
 
